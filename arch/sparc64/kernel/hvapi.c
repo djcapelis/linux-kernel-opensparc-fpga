24db5954858bb1773f038430fd625652261665fa
D J Capelis <dev@capelis.dj>
2013-04-16 20:06:43 -0700

    Adding patches from the OpenSPARC T1 tree
    These didn't apply cleanly to the 2.6.22 tree, so I applied some chunks
    by hand.  I also modified the define to actually work using the kernel
    configuration system instead of just defining it at the top of the file
    like the original patch does.  This should be a lot cleaner and should
    allow this tree to work on environments not on an FPGA as well with the
    simple flick of an configuration option.
    
    This new option is CONFIG_OPENSPARC_T1_FPGA

--- arch/sparc64/kernel/hvapi.c
+++ arch/sparc64/kernel/hvapi.c
@@ -180,6 +180,14 @@ void __init sun4v_hvapi_init(void)
 	if (sun4v_hvapi_register(group, major, &minor))
 		goto bad;
 
+#ifdef CONFIG_OPENSPARC_T1_FPGA
+         group = HV_GRP_INTR;
+         major = 1;
+         minor = 0;
+         if (sun4v_hvapi_register(group, major, &minor))
+                 goto bad;
+#endif
+
 	sun4v_sstate_init();
 
 	return;
