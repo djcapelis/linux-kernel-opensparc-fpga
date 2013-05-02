ca322acc804aa4881dcea6095132763ab8cf9b3f
D J Capelis <dev@capelis.dj>
2013-05-01 18:47:54 -0700

    Update xccmake.sh with new ARCH options and specify CFLAGS
    ARCH is now just sparc, not sparc64 and add -mcpu=niagara to CFLAGS
    because why not?

--- xccmake.sh
+++ xccmake.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-make ARCH=sparc64 CROSS_COMPILE=sparc64-linux-gnu- INSTALL_MOD_PATH=/home/djc/src/lockbox-hw-kernel-sparc/modules_to_fpga $*
+make ARCH=sparc CROSS_COMPILE=sparc64-linux-gnu- CFLAGS=-mcpu=niagara INSTALL_MOD_PATH=/home/djc/src/lockbox-hw-kernel-sparc/modules_to_fpga $*
